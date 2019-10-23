9b83a53ffa649e26f4688a9d92876159b40f657d
danil <antonovdanil2000@gmail.com>
2019-10-23 20:32:56 +0300

    For test smart selects on pythoneverywhere

--- NURE_marks_engine/settings.py
+++ NURE_marks_engine/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2nn#j#uut-(4a*ghb%04urb@+aphyysy*az6fiy4+@jz!56b1h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # ALLOWED_HOSTS = []
 
 # Application definition
@@ -188,5 +188,7 @@ SOCIALACCOUNT_ADAPTER = "NURE_marks.adapters_for_Google_API.MySocialAccount"
 # ACCOUNT_AUTHENTICATION_METHOD = "email"
 #
 # ACCOUNT_EMAIL_REQUIRED = True
+
+USE_DJANGO_JQUERY = True
 
 
